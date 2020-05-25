1fb6fd89d005c88d52060dfccf424c31962f7f3b
Timothy John Perisho Eccleston <combinatorist@users.noreply.github.com>
2020-05-25 18:36:02 -0500

    typo in python-packages.nix

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1,7 +1,7 @@
 # This file contains the Python packages set.
 # Each attribute is a Python library or a helper function.
 # Expressions for Python libraries are supposed to be in `pkgs/development/python-modules/<name>/default.nix`.
-# Python packages that do not need to be available for each interpreter version do not belong in this packages set.
+# Python packages that do not need to be available for each interpreter version do not belong in this package's set.
 # Examples are Python-based cli tools.
 #
 # For more details, please see the Python section in the Nixpkgs manual.
